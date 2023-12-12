bca9fa652bb2b7a0fb1bbc7bb4984b6057b51ec8
baretkuznik <bartek.krecio@onet.pl>
2023-12-12 15:57:45 +0100

    cors

--- Room_Server/Room_Server/settings.py
+++ Room_Server/Room_Server/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ALLOWED_CREDENTIALS = True
 
 # Application definition
 
@@ -39,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'app2',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
